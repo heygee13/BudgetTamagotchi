9ede8c4a01971259c5df4a074da902edc2d501ae
Gatha Mukhopadhyay <phdigitalnix@gmail.com>
2025-09-08 16:12:40 +0530

    initial Vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🐹 Budget Tamagotchi 🐹</h1>
+      <p>Your Tamagotchi is born! Feed it some code 💻</p>
+    </div>
+  );
+}
+
+export default App;
